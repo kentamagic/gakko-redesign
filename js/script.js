f65245b478d4c74e58432d35779b2433352992b1
Seth Thompson <s3th.thompson@gmail.com>
2013-06-15 19:03:14 -0400

    class name change

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ $(function(){
 function play(){
 	var embed = ' <iframe src="http://player.vimeo.com/video/58013264?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;autoplay=1" width="1280" height="720" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
 	
-	var $video = $("#hero-video");
+	var $video = $(".hero-vid");
 	$video.html(embed)
 		.fitVids()
 		.fadeIn();
 	
-	$("#hero").hide();
+	$(".hero-img").hide();
 }
 
