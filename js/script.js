4f62419ff08416637a95c9d7c0f77a859d6d43a0
Kenta Koga <kentamagic@gmail.com>
2013-06-15 18:58:03 -0400

    started writing script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
-  var embed = ' <iframe src="http://player.vimeo.com/video/58013264?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;autoplay=1" width="1280" height="720" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
-  $("#hero-vid").fitVids();
+$(function(){
+  $("#playVid").click(play);
 });
+
+function play(){
+	var embed = ' <iframe src="http://player.vimeo.com/video/58013264?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff&amp;autoplay=1" width="1280" height="720" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+	
+	var $video = $("#hero-video");
+	$video.html(embed)
+		.fitVids()
+		.fadeIn();
+	
+	$("#hero").hide();
+}
+
